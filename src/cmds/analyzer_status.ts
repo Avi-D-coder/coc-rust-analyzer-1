9800e660d98b37330d9de578b134754d4bfc6f82
Avi Dessauer <avi.the.coder@gmail.com>
2019-11-19 23:40:08 -0500

    Delete everything

--- src/cmds/analyzer_status.ts
+++
@@ -1,7 +0,0 @@
-import { workspace } from 'coc.nvim';
-import { Server } from '../server';
-
-export async function handler() {
-  const ret = await Server.client.sendRequest<string>('rust-analyzer/analyzerStatus', null);
-  workspace.echoLines(ret.split('\n'));
-}
